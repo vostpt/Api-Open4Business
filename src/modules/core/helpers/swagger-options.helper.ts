dfe92ea813e27407b7880340872202eeb5284a04
Pedro Santos <pedro.santos@domatica.pt>
2020-04-29 21:26:24 +0100

    add custom icon to documentation

--- src/modules/core/helpers/swagger-options.helper.ts
+++ src/modules/core/helpers/swagger-options.helper.ts
@@ -5,8 +5,8 @@ export class SwaggerOptionsHelper {
   static getDefaultImage() {
     let customCss = '';
     try {
-      const img = readFileSync(join(__dirname, '../../../assets/images/default-domain-image.png')).toString('base64');
-      customCss = `.topbar-wrapper img {content:url(data:image/png};base64,${img}); height: 50px;} .swagger-ui .topbar {padding: 10px 0 6px 0;background-color: #fafafa;}`;
+      const img = readFileSync(join(__dirname, '../../../assets/images/logo.png')).toString('base64');
+      customCss = `.topbar-wrapper img {content:url(data:image/png};base64,${img});} .swagger-ui .topbar {padding: 10px 0 6px 0;background-color: #fafafa;}`;
     } catch (e) {
       // nothing happens
     }
